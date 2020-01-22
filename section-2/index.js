f37aed0f4d2b09d851436d683dc9170773674c36
Eder Canada <canada.eder@gmail.com>
2020-01-22 15:32:21 -0800

    big picture

---
+++ section-2/index.js
@@ -0,0 +1,17 @@
+//////////////////////////////////////////// BIG PICTURE //////////////////////////////////
+
+//React.js is a  JS library that is used to create UI. Working only with React, I am not able to excute logic in the server. 
+
+//Server is created using Node.js and the node framework express.js, it is seperate from the client in the browser so any business logic can be ran here that I want to hide from the user. Node and Express can be used for file storage. 
+
+//The server sends a response and the server sends back a response. The data is sent via JSON format, for both a request from the client and response from the server.
+
+//In the MERN stack, there are two serves one for Node and one for MongoDB. Client side is React only. File storage is done by Node and MongoDB stores users, products, ect.
+
+//Whos is talking to the DB server? That is the Node Express app. Node send DB queries. I never send request from the client to MongoDB, because to send those queries I need to send my credentials to login into my datebase. I only send request from Node to MongoDB.
+
+
+
+
+
+
