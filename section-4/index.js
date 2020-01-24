b7837e0cc975de44317e40ebc7836274000d25a3
Eder Canada <canada.eder@gmail.com>
2020-01-24 08:47:38 -0800

    set up basic react app

---
+++ section-4/index.js
@@ -0,0 +1,19 @@
+// JSX lets me write HTML in my React app
+
+// What makes up a react component? - A react component can be one of two things, A function that returns JSX or it can be a JS class that has a render method. 
+
+//FUNCTION COMPONENT
+const app = () => {
+  return <h1> hi </h1>
+}
+
+//CLASS COMPONENT
+class App extends React.Component {
+  render () {
+    return <h1> hi</h1>
+  }
+}
+
+// css classes do not exist in React JSX, so use className="" instead
+
+// every component should just do one thing.
---
+++ section-4/react-refresher/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; 
+
+const App = () => {
+  return (
+  <div className="course-goals">
+    <h2> Course Goals </h2>
+    <ul className="goal-list">
+      <li> Finish the Course</li>
+      <li>  Learn all course main topic</li>
+      <li> Build more MERN projects </li>
+    </ul>
+  </div>
+  )
+}
+export default App;
